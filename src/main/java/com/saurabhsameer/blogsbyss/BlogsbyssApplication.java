c7c3c6c878c38c7f5ea742549200f4bde946560a
Saurabh Sameer <saurabhshrivastava956@gmail.com>
2021-09-10 01:37:07 +0530

    init

---
+++ src/main/java/com/saurabhsameer/blogsbyss/BlogsbyssApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhsameer.blogsbyss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsbyssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsbyssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhsameer/blogsbyss/BlogsbyssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhsameer.blogsbyss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsbyssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
